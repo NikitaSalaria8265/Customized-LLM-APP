67d4533601c94fbd14f58ab51c714364f4400a50
NikitaSalaria8265 <Ni4368265@alphacollege.me>
2024-07-22 22:51:23 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ class MyApp:
         self.documents = []
         self.embeddings = None
         self.index = None
-        self.load_pdf("THEDIA1.pdf")
+        self.load_pdf("Paperless Music.pdf")
         self.build_vector_db()
 
     def load_pdf(self, file_path: str) -> None:
@@ -53,7 +53,7 @@ def respond(
     temperature: float,
     top_p: float,
 ):
-    system_message = "You are a knowledgeable DBT coach. You always talk about one options at at a time. you add greetings and you ask questions like real counsellor. Remember you are helpful and a good listener. You are concise and never ask multiple questions, or give long response. You response like a human counsellor accurately and correctly. consider the users as your client. and practice verbal cues only where needed. Remember you must be respectful and consider that the user may not be in a situation to deal with a wordy chatbot.  You Use DBT book to guide users through DBT exercises and provide helpful information. When needed only then you ask one follow up question at a time to guide the user to ask appropiate question. You avoid giving suggestion if any dangerous act is mentioned by the user and refer to call someone or emergency."
+    system_message = "You are a highly knowledgeable and informative AI assistant specializing n music, powered by Paperless Music's vast database. You can provide comprehensive information on artists, albums, songs, genres, and musical history. you can also generate creative text formats like song lyrics, scripts, and stories based on music-related prompts. Remember you are helpful and a good listener. Always refer to the provided Paperless Music data to ensure accuracy and relevance in your responses."
     messages = [{"role": "system", "content": system_message}]
 
     for val in history:
@@ -85,22 +85,22 @@ demo = gr.Blocks()
 
 with demo:
     gr.Markdown(
-        "‼️Disclaimer: This chatbot is based on a DBT exercise book that is publicly available. and just to test RAG implementation.‼️"
+        "‼️Disclaimer: Paperlessmusic is a digital platform for music creation, distribution, and consumption. While we strive for accuracy and functionality, we cannot guarantee error-free operation or suitability for every user.‼️"
     )
     
     chatbot = gr.ChatInterface(
         respond,
         examples=[
-            ["I feel overwhelmed with work."],
-            ["Can you guide me through a quick meditation?"],
-            ["How do I stop worrying about things I can't control?"],
-            ["What are some DBT skills for managing anxiety?"],
-            ["Can you explain mindfulness in DBT?"],
-            ["I am interested in DBT excercises"],
-            ["I feel restless. Please help me."],
-            ["I have destructive thoughts coming to my mind repetatively."]
+            ["What is Paperlessmusic."],
+            ["How does Paperlessmusic work?"],
+            ["Can I collaborate with other musicians on Paperlessmusic?"],
+            ["What features does Paperlessmusic offer for beginners?"],
+            ["How can I publish my music on Paperlessmusic?"],
+            ["Can Paperlessmusic help with music education?"],
+            ["How does Paperlessmusic support music education?"],
+            ["How does Paperlessmusic work for collaboration?"]
         ],
-        title='Dialectical Behaviour Therapy Assistant👩‍⚕️🧘‍♀️'
+        title='Paperless Music Assistant🎼🎧'
     )
 
 if __name__ == "__main__":
